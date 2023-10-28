05f6eddf9f7fc60e7a74e49db06f4fec65e90c67
DEVIL <122738174+DEVILxD5919@users.noreply.github.com>
2023-10-28 11:05:48 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# FrostMaintenanceMode
+# Information 
+ - When Maintenance Mode is Active, Only Players With OP or The Permission "maintenancemode.bypass" Can Join Your Server.
+ - Maintenance Mode Kick Message is Configurable Via The Conig File.
+# Commands
+- /mm on
+- /mm off
+# Permissions
+- Permission: maintenancemode.bypass
+# Credits
+- Icon made by Freepik from www.flaticon.com
---
+++ src/DEVIL/FrostMaintenanceMode/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace DEVIL\FrostMaintenanceMode;
+
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+class Main extends PluginBase implements Listener{
+
+    public function onEnable() : void
+    {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onJoin(PlayerLoginEvent $event){
+        $player = $event->getPlayer();
+
+        if ($this->getConfig()->get("Maintenance_Mode_Active") === true && !$player->hasPermission("maintenancemode.bypass")){
+            $player->kick($this->getConfig()->get("Maintenance_Mode_Message"), false);
+        }
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        if (!$sender->hasPermission("maintenancemode.cmd")){
+            $sender->sendMessage("§cYou don't have permissions to use this command");
+            return true;
+        }
+        if (!isset($args[0])){
+            $sender->sendMessage("§cUsage: §a/mm on | off");
+            return true;
+        }
+        switch($command->getName()){
+            case "mm":
+                switch (strtolower($args[0])){
+                    case "on":
+                        $this->getConfig()->set("Maintenance_Mode_Active", true);
+                        $this->getConfig()->save();
+                        $sender->sendMessage("§aYou have turned on maintenance mode");
+                    break;
+                    case "off":
+                        $this->getConfig()->set("Maintenance_Mode_Active", false);
+                        $this->getConfig()->save();
+                        $sender->sendMessage("§aYou have turned off maintenance mode");
+                    break;
+                }
+
+        }
+        return true;
+    }
+}
